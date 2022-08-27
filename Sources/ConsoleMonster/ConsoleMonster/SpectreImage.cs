ab288dc4a854e9066ce7c5b2b28d1cee21d3728e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-08-27 20:24:46 +0200

    Update

---
+++ Sources/ConsoleMonster/ConsoleMonster/SpectreImage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace ConsoleMonster {
+    [Cmdlet(VerbsCommon.New, "SpectreImage")]
+    [OutputType(typeof(void))]
+    public class SpectreImage : PSCmdlet {
+        [Parameter(
+            Mandatory = true
+        )]
+        [AllowNull]
+        [AllowEmptyString]
+        public string FilePath { get; set; }
+
+        // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
+        protected override void EndProcessing() {
+            WriteObject("test");
+        }
+    }
+}
