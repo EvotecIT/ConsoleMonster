6d747bd68ae21f018dd9ad1417693dbb5ab66569
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-08-27 19:46:22 +0200

    new function

---
+++ Public/New-SpectreImage.ps1
@@ -0,0 +1,12 @@
+﻿function New-SpectreImage {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)][string] $FilePath,
+        [int] $MaxWidth
+    )
+    $Image = [Spectre.Console.CanvasImage]::new($FilePath)
+    if ($MaxWidth) {
+        $Image.MaxWidth = $MaxWidth
+    }
+    [Spectre.Console.AnsiConsole]::Write($Image)
+}
