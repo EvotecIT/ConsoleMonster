fdb7336c76b2b7b47942b0d5e1a8d8648698cbcb
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-08-27 17:46:39 +0200

    Add example

--- Examples/Example-SpectreRules.ps1
+++ Examples/Example-SpectreRules.ps1
@@ -1,4 +1,4 @@
-﻿#Import-Module .\ConsoleMonster.psd1 -Force
+﻿Import-Module .\ConsoleMonster.psd1 -Force
 
 New-SpectreRule -Text 'This is my rules!', 'centered' -Color yellow, green -RuleColor blue
 New-SpectreRule -Text 'This is my rules!', 'left aligned' -Color yellow, green -RuleColor blue -Align Left
