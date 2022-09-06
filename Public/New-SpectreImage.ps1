dcb918729dfa41994d70eded5a825f3b9559b383
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-09-06 09:10:05 +0200

    Update spectre image to support Url

--- Public/New-SpectreImage.ps1
+++ Public/New-SpectreImage.ps1
@@ -1,10 +1,25 @@
 ﻿function New-SpectreImage {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory)][string] $FilePath,
+        [Parameter(Mandatory, ParameterSetName = 'File')][string] $FilePath,
+        [Parameter(Mandatory, ParameterSetName = 'Url')][alias()][Uri] $Uri,
         [int] $MaxWidth
     )
-    $Image = [Spectre.Console.CanvasImage]::new($FilePath)
+    if ($Uri) {
+        $Extension = [IO.Path]::GetExtension($Uri.AbsolutePath)
+        $FilePath = [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), "$($([System.IO.Path]::GetRandomFileName()).Split('.')[0]).$Extension")
+        $ProgressPreference = 'SilentlyContinue'
+        Try {
+            Invoke-WebRequest -Uri $Uri -OutFile $FilePath -ErrorAction Stop
+        } Catch {
+            Write-Warning -Message "Failed to download image from $($Uri.AbsoluteUri)"
+            Return
+        }
+
+    }
+    if ($FilePath -and (Test-Path -Path $FilePath)) {
+        $Image = [Spectre.Console.CanvasImage]::new($FilePath)
+    }
     if ($MaxWidth) {
         $Image.MaxWidth = $MaxWidth
     }
