74d8de62243a43c93b79b06cb7b3a0863478dee8
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-09-06 09:10:48 +0200

    Add an example

--- Examples/Example-SpectreImage.ps1
+++ Examples/Example-SpectreImage.ps1
@@ -2,3 +2,4 @@
 
 New-SpectreImage -FilePath "$PSScriptRoot\Images\Kulek.jpg"
 New-SpectreImage -FilePath "$PSScriptRoot\Images\PrzemyslawKlysAndKulkozaurr.jpg"
+New-SpectreImage -Uri 'https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png'
