565f4ad71594eef72bca1e87492b22a41fb97455
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-09-06 09:10:56 +0200

    Example

--- Examples/Example-Terminal.ps1
+++ Examples/Example-Terminal.ps1
@@ -2,6 +2,16 @@
 
 New-Terminal {
     New-TerminalWindow -Title 'Test' {
-        New-TerminalText -Text 'Special Text' -Width 20
+        New-TerminalText -Text 'Special Text' -Width 20 -X 20
+        New-TerminalButton -Text "Quit" -X 2 -Y 20 -Command {
+
+        }
+        New-TerminalRadioGroup -Text "one", 'Two', 'Three' -X 10 -Y 30
+
+
+        New-TerminalCheckBox -Text "CheckBox1" -X 10 -Y 50
+        New-TerminalCheckBox -Text "CheckBox2" -X 10 -Y 60
+        New-TerminalCheckBox -Text "CheckBox3" -X 10 -Y 70
+
     }
 }
