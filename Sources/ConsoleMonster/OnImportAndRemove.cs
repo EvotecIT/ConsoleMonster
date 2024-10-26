badcaf48dfe21518184a37a3a377faa4ab9728ca
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-10-26 12:17:24 +0200

    Make sure to use the same as other projects

--- Sources/ConsoleMonster/OnImportAndRemove.cs
+++ Sources/ConsoleMonster/OnImportAndRemove.cs
@@ -2,44 +2,57 @@ using System;
 using System.IO;
 using System.Management.Automation;
 using System.Reflection;
+using System.Collections.Generic;
 
 public class OnModuleImportAndRemove : IModuleAssemblyInitializer, IModuleAssemblyCleanup {
     public void OnImport() {
-#if FRAMEWORK
-        AppDomain.CurrentDomain.AssemblyResolve += MyResolveEventHandler;
-#endif
+        if (IsNetFramework()) {
+            AppDomain.CurrentDomain.AssemblyResolve += MyResolveEventHandler;
+        }
     }
 
     public void OnRemove(PSModuleInfo module) {
-#if FRAMEWORK
-        AppDomain.CurrentDomain.AssemblyResolve -= MyResolveEventHandler;
-#endif
+        if (IsNetFramework()) {
+            AppDomain.CurrentDomain.AssemblyResolve -= MyResolveEventHandler;
+        }
     }
 
     private static Assembly MyResolveEventHandler(object sender, ResolveEventArgs args) {
-        // These are known to be problematic in .NET Framework, force it to use our packaged dlls.
-        if (args.Name.StartsWith("System.Memory,")) {
-            string binPath = Path.Combine(Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location), "System.Memory.dll");
-            return Assembly.LoadFile(binPath);
-        } else if (args.Name.StartsWith("System.Runtime.CompilerServices.Unsafe,")) {
-            string binPath = Path.Combine(Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location), "System.Runtime.CompilerServices.Unsafe.dll");
-            return Assembly.LoadFile(binPath);
-        } else if (args.Name.StartsWith("System.Numerics.Vectors,")) {
-            string binPath = Path.Combine(Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location), "System.Numerics.Vectors.dll");
-            return Assembly.LoadFile(binPath);
-        } else if (args.Name.StartsWith("System.Drawing.Common,")) {
-            string binPath = Path.Combine(Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location), "System.Drawing.Common.dll");
-            return Assembly.LoadFile(binPath);
-        } else if (args.Name.StartsWith("System.Buffers,")) {
-            string binPath = Path.Combine(Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location), "System.Buffers.dll");
-            return Assembly.LoadFile(binPath);
-        } else if (args.Name.StartsWith("System.ValueTuple,")) {
-            string binPath = Path.Combine(Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location), "System.ValueTuple.dll");
-            return Assembly.LoadFile(binPath);
-        } else if (args.Name.StartsWith("System.Text.Encoding.CodePages,")) {
-            string binPath = Path.Combine(Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location), "System.Text.Encoding.CodePagesdll");
-            return Assembly.LoadFile(binPath);
+        var libDirectory = Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location);
+        var directoriesToSearch = new List<string> { libDirectory };
+
+        if (Directory.Exists(libDirectory)) {
+            directoriesToSearch.AddRange(Directory.GetDirectories(libDirectory, "*", SearchOption.AllDirectories));
         }
+
+        var requestedAssemblyName = new AssemblyName(args.Name).Name + ".dll";
+
+        foreach (var directory in directoriesToSearch) {
+            var assemblyPath = Path.Combine(directory, requestedAssemblyName);
+
+            if (File.Exists(assemblyPath)) {
+                try {
+                    return Assembly.LoadFrom(assemblyPath);
+                } catch (Exception ex) {
+                    Console.WriteLine($"Failed to load assembly from {assemblyPath}: {ex.Message}");
+                }
+            }
+        }
+
         return null;
+    }
+
+    private bool IsNetFramework() {
+        return System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET Framework", StringComparison.OrdinalIgnoreCase);
+    }
+    private bool IsNetCore() {
+        return System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET Core", StringComparison.OrdinalIgnoreCase);
+    }
+    private bool IsNet5OrHigher() {
+        return System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET 5", StringComparison.OrdinalIgnoreCase) ||
+               System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET 6", StringComparison.OrdinalIgnoreCase) ||
+               System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET 7", StringComparison.OrdinalIgnoreCase) ||
+               System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET 8", StringComparison.OrdinalIgnoreCase) ||
+               System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET 9", StringComparison.OrdinalIgnoreCase);
     }
 }
